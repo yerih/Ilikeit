b2fd52e0f49cb1d234d3c3981b5b8d0168bc6d9b
yerih <yerih7tup@gmail.com>
2023-09-02 03:01:22 -0400

    gradle update. graphQL's not working.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,13 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.1.0")
+        classpath("com.android.tools.build:gradle:7.1.3")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
         classpath("com.google.dagger:hilt-android-gradle-plugin:2.42")
+//        classpath("com.apollographql.apollo:apollo-gradle-plugin:3.7.3")
     }
 }
+//apply(plugin = "com.apollographql.apollo")
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
